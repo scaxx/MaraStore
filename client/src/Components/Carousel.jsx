f7eb7fe55b28b7d21b829a3b523555dc9c9431cd
Sofía <danessesofia@gmail.com>
2025-09-25 19:55:35 -0300

    Agregando carrusel, arreglos de estilo

---
+++ client/src/Components/Carousel.jsx
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from 'react'
+
+const Carousel = () => {
+    //Índice de la imagen actual
+    const [current, setCurrent] = useState(0);
+
+    //Imágenes
+    const imagenes = [
+        "./src/assets/Muestras1.png",
+        "./src/assets/Muestras2.png"
+    ];
+
+    //Cambia la foto
+    useEffect(() => {
+        const intervalo = setInterval(() => {
+            setCurrent((prev) => (prev === imagenes.length - 1 ? 0 : prev + 1));
+        }, 10000);
+
+        return () => clearInterval(intervalo);
+    }, [imagenes.length]);
+
+  return (
+    <div className='relative w-full max-w-2xl mx-auto overflow-hidden rounded-xl shadow-lg'>
+        <div 
+            className='flex transition-transform duration-700'
+            style={{ transform: `translateX(-${current * 100}%)` }}
+        >
+            {imagenes.map((src, index) => (
+                <img
+                    key={index}
+                    src={src} 
+                    alt={`slide-${index}`}
+                    className='w-full flex-shrink-0 object-cover'
+                />
+            ))}
+        </div>        
+    </div>
+  )
+}
+
+export default Carousel
--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
+import Carousel from '../Components/Carousel'
 
 function Home() {
 
   return (
-    <div>
-      <div>Foto</div>
-      <h1 class="text-3xl font-bold underline">MaraStore</h1>
+    <div className='flex flex-col gap-20 place-items-center'>
+      <div className='flex flex-row gap-50 place-items-center'>
+        <Carousel />
+        <div className='flex flex-col place-items-center'>
+          <h3 className='text-7xl'>Bienvenido a</h3>
+          <h1 className='text-9xl font-bold cursor-pointer text-lime-600'>MaraStore</h1>
+        </div>
+      </div>
+      <button>Button</button>
     </div>
   )
 }
