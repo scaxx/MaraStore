59f0b203ee3ef742a8048036e86bed501d44ee2e
Sofía <danessesofia@gmail.com>
2025-09-25 21:07:04 -0300

    Primeros ajustes de SearchBar

--- client/src/Components/SearchBar.jsx
+++ client/src/Components/SearchBar.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import Button from './Button'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
 
 const SearchBar = () => {
   return (
-    <div>SearchBar</div>
+    <>
+      <input type="text" className='bg-zinc-700 rounded-xl' placeholder='Busca un producto...'/>
+      <Button onClick={alert("Buscar")} variante='icon'>
+        <FontAwesomeIcon icon={faMagnifyingGlass} />
+      </Button>
+    </>
   )
 }
 
