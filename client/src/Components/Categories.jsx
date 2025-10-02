847b3dba1c061a68b71b353a4044ad4854783efa
Sofía <danessesofia@gmail.com>
2025-10-01 22:46:39 -0300

    Ajustes

--- client/src/Components/Categories.jsx
+++ client/src/Components/Categories.jsx
@@ -1,13 +1,28 @@
 import React from 'react'
 
 const Categories = () => {
+  const categories = [
+    'Laptops',
+    'Smartphones',
+    'Accesorios',
+    'Tablets',
+    'Consolas',
+    'TVs',
+    'Cámaras',
+    'Audio',
+    'Monitores',
+    'Smartwatch',
+    'Drones',
+    'Periféricos'
+  ]
+
   return (
-    <div className='bg-zinc-700 p-6 grid grid-cols-3 place-items-center gap-6 rounded-xl text-2xl'>
-      {['Hola 1', 'Hola 2', 'Hola 3', 'Hola 4', 'Hola 5', 'Hola 6'].map((item, index) => (
+    <div className='bg-zinc-700 p-6 grid grid-cols-3 place-items-center gap-6 rounded-xl text-xl animate-slideFadeDown w-[800px]'>
+      {categories.map((item, index) => (
         <p
           key={index}
           onClick={() => alert(item)}
-          className='bg-zinc-700 hover:bg-zinc-800 transform hover:-translate-y-1 transition-all duration-300 p-4 rounded-xl cursor-pointer w-full text-center'
+          className='bg-zinc-800 hover:bg-zinc-900 transform hover:-translate-y-1 transition-all duration-300 p-3 rounded-xl cursor-pointer w-full text-center shadow-md'
         >
           {item}
         </p>
--- client/src/Components/Shop.jsx
+++ client/src/Components/Shop.jsx
@@ -11,16 +11,27 @@ import Categories from './Categories'
 const Shop = () => {
   const [open, setOpen] = useState(false);
 
-    return (
+  return (
     <div className='shop-container flex flex-col gap-10 place-items-center m-10'>
-        <div className='flex flex-row gap-10 place-items-center'>
-            <SearchBar />
-            <Button variante='menu' onClick={() => setOpen(!open)}>
-              Categorías
-              <FontAwesomeIcon icon={faChevronDown} className={`transform transition-transform duration-500 ${open ? 'rotate-180' : 'rotate-0'}`}/> 
-            </Button>
+      <div className='flex flex-row gap-10 place-items-center relative'>
+        <SearchBar />
+
+        <div className="relative">
+          <Button variante='menu' onClick={() => setOpen(!open)}>
+            Categorías
+            <FontAwesomeIcon
+              icon={faChevronDown}
+              className={`transform transition-transform duration-500 ${open ? 'rotate-180' : 'rotate-0'}`}
+            />
+          </Button>
+
+          {open && (
+            <div className="absolute left-1/2 transform -translate-x-1/2 top-full mt-4 z-50">
+              <Categories />
+            </div>
+          )}
         </div>
-        {open && <Categories />}
+      </div>
     </div>
   )
 }
