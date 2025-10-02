5637e668902c622040b6a4617b9c520fa017c678
Sofía <danessesofia@gmail.com>
2025-10-02 20:31:45 -0300

    Mejora de la animación de categories

--- client/src/Components/Shop.jsx
+++ client/src/Components/Shop.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import "../../src/index.css"
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -10,13 +10,27 @@ import Categories from './Categories'
 
 const Shop = () => {
   const [open, setOpen] = useState(false)
+  const categoriesRef = useRef(null);
+
+  useEffect(() => {
+    const handleClickOutside = (e) => {
+      if (categoriesRef.current && !categoriesRef.current.contains(e.target)) {
+        setOpen(false);
+      }
+    }
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    }
+  }, [])
 
   return (
     <div className='shop-container flex flex-col gap-10 place-items-center m-10'>
       <div className='flex flex-row gap-10 place-items-center'>
         <SearchBar />
 
-        <div className="relative">
+        <div className="relative" ref={categoriesRef}>
           <Button variante='menu' onClick={() => setOpen(!open)}>
             Categorías
             <FontAwesomeIcon
